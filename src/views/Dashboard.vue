b74b24608a1f5e72793d824d463f745ffc142460
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-11 13:41:56 -0600

    add rss feeds for rotowire, nfl, nba, and featured news.  calling from backend and displaying on front end working.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+     <VueRssFeed :feedUrl="nflRotoworldFeed" :name="name" :limit="limit"/>
+     <VueRssFeed :feedUrl="nbaRotoworldFeed" :name="name" :limit="limit"/>
+     <VueRssFeed :feedUrl="rotoworldFeed" :name="name" :limit="limit"/>
   </div>
 </template>
 
 <style></style>
 
 <script>
+  import VueRssFeed from "vue-rss-feed";
   export default {
+    name: "Demo",
+    components: {
+    VueRssFeed
+  },
     data: function () {
       return {
         message: "This is the dashboard",
+        nflRotoworldFeed: "http://localhost:3000/rotowire_nfl_news",
+        name: "",
+        limit: 5,
+        nbaRotoworldFeed: "http://localhost:3000/rotowire_nba_news",
+        name: "",
+        limit: 5,
+        rotoworldFeed: "http://localhost:3000/rotowire_news",
+        name: "",
+        limit: 5
       };
     },
     created: function () {},
