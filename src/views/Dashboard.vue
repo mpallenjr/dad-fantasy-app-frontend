83c7a3cd71c35974384f3fa69b294b6e3fa65215
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-11 13:26:50 -0600

    further setup of dashboard and boilder plate for views.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -10,7 +10,7 @@
   export default {
     data: function () {
       return {
-        message: "This is the home, sign in/sign up page",
+        message: "This is the dashboard",
       };
     },
     created: function () {},
