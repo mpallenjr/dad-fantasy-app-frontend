8069cdedadb4f214c9081da516ef715310d72407
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-11 14:07:57 -0600

    all rss feeds added from backend to frontend.  all working.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,6 +4,14 @@
      <VueRssFeed :feedUrl="nflRotoworldFeed" :name="name" :limit="limit"/>
      <VueRssFeed :feedUrl="nbaRotoworldFeed" :name="name" :limit="limit"/>
      <VueRssFeed :feedUrl="rotoworldFeed" :name="name" :limit="limit"/>
+     <VueRssFeed :feedUrl="ffaNewsFeed" :name="name" :limit="ffalimit"/>
+     <VueRssFeed :feedUrl="dynastyNerdsNewsFeed" :name="name" :limit="limit"/>
+     <VueRssFeed :feedUrl="sleeperUNewsFeed" :name="name" :limit="sleeperUlimit"/>
+     <VueRssFeed :feedUrl="playerProfilerFeed" :name="name" :limit="playerProfilerlimit"/>
+     <VueRssFeed :feedUrl="hoopsHypeNewsFeed" :name="name" :limit="hoopsHypeNewslimit"/>
+     <VueRssFeed :feedUrl="hoopDoctorsNewsfeed" :name="name" :limit="hoopDoctorsNewslimit"/>
+     <VueRssFeed :feedUrl="basketballInsidersNewsfeed" :name="name" :limit="basketballInsidersNewslimit"/>
+
   </div>
 </template>
 
@@ -23,11 +31,21 @@
         name: "",
         limit: 5,
         nbaRotoworldFeed: "http://localhost:3000/rotowire_nba_news",
-        name: "",
-        limit: 5,
         rotoworldFeed: "http://localhost:3000/rotowire_news",
-        name: "",
-        limit: 5
+        ffaNewsFeed: "http://localhost:3000/ffa_news",
+        ffalimit: 1,
+        dynastyNerdsNewsFeed: "http://localhost:3000/dynasty_nerds_news",
+        sleeperUNewsFeed: "http://localhost:3000/sleeper_u_news",
+        sleeperUlimit: 3,
+        playerProfilerFeed: "http://localhost:3000/player_profiler_news",
+        playerProfilerlimit: 8,
+        hoopsHypeNewsFeed: "http://localhost:3000/hoops_hype_news",
+        hoopsHypeNewslimit: 10,
+        hoopDoctorsNewsfeed: "http://localhost:3000/hoop_doctors_news",
+        hoopDoctorsNewslimit: 1,
+        basketballInsidersNewsfeed: "http://localhost:3000/basketball_insiders_news",
+        basketballInsidersNewslimit: 10
+
       };
     },
     created: function () {},
