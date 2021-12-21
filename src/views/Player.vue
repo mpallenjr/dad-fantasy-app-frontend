441853dd2e6898aaac290cdc2b151f499d4e109a
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-12-20 19:51:46 -0600

    ui updates.

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,48 +1,54 @@
 <template>
-  <div class="home">
-   first name:  <input type="text" class="center" v-model="inputPlayerData.firstName"> last name:  <input type="text" class="center" v-model="inputPlayerData.lastName">
-   <br/>
-   <br/>
-   <br/>
-   <button class="center" v-on:click="searchPlayers()">Search</button>
-   <hr/>
-   
-    
-<form>
-<h3>Recent Tweets</h3>
-									<div  v-for="tweet in inputPlayerTweetResults">
-										<table>
-											<!-- <thead>
-												<tr>
-													<th>Recent Tweets</th>
-												</tr>
-											</thead> -->
-											<tbody>
-												<tr>							
-													<td>{{tweet.text}}</td>	
-												</tr>			
-											</tbody>
-										</table>
-									</div>
-                  <div v-for="result in inputPlayerResults" class="headlines" >
-                  <h2>{{result.title}} | {{result.author}} - {{result.source.name}} </h2>
-									<p><span class="image left"><img v-bind:src="result.urlToImage" v-bind:alt="result.description" width=125 height=125/></span>{{result.description}}</p>
-                  <br />
-                  <br />
-                  <br />
-                  </div>
-</form>
+  <div id="main">
+    <br/>
+    <section>
+      <h3>Player Search</h3>
+      <form method="post" action="#">
+        <div class="row gtr-uniform">
+          <div class="col-6 col-12-xsmall">
+            <input type="text" name="name" id="first-name" value="" placeholder="First Name" v-model="inputPlayerData.firstName" />
+          </div>
+          <div class="col-6 col-12-xsmall">
+            <input type="text" name="name" id="last-name" value="" placeholder="Last Name" v-model="inputPlayerData.lastName" />
+          </div>
+        </div>
+      </form>
+    </section>
+    <button class="center" v-on:click="searchPlayers()">Search</button>
+    <hr/>
 
-							
+    <form>
+      <h3>Recent Tweets</h3>
+      <div  v-for="tweet in inputPlayerTweetResults">
+        <table>
+          <!-- <thead>
+            <tr>
+              <th>Recent Tweets</th>
+            </tr>
+          </thead> -->
+          <tbody>
+            <tr>							
+              <td>{{tweet.text}}</td>	
+            </tr>			
+          </tbody>
+        </table>
+      </div>
+      <div v-for="result in inputPlayerResults" class="headlines" >
+      <h2>{{result.title}} | {{result.author}} - {{result.source.name}} </h2>
+      <p><span class="image left"><img v-bind:src="result.urlToImage" v-bind:alt="result.description" width=125 height=125/></span>{{result.description}}</p>
+      <br />
+      <br />
+      <br />
+      </div>
+    </form>							
   </div>
 </template>
 
 <style>
 
-headlines
-{
-text-align:center;
-text-justify:values;
+input {
+    width: 250px;
+    padding: 5px;
 }
 </style>
 
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="home">
+        <img src="/images/Subscribe.png" width=800 height=800 class="center">
+
     <div id="contact">
       <p> <form id="fs-frm" name="registration-form" accept-charset="utf-8" action="https://formspree.io/f/mayvylad" method="post" class="center">
         <fieldset id="fs-frm-inputs">
-        <br />
-        <br />
-        <br />
-        <label for="email-address" class="center">Subscribe for Updates on the App!</label>
-        <br />
         <input type="email" name="_replyto" id="email-address" placeholder="email@address.com" required="">
         <input type="hidden" name="_subject" id="email-subject" value="Registration Form Submission">
         </fieldset>
-        <input type="submit" value="Subscribe" class="center">
+        <input type="submit" value="Tune In" class="center">
       </form> </p>
     </div>
+            <img src="/images/tune-in.png" width=800 height=800 class="center">
+
   </div>
 </template>
 
